3ef1a06c3b770d4af2dca81b35be663bbe5cf17d
dhongle <105487585+dhongle@users.noreply.github.com>
2023-07-27 10:32:16 +0200

    Add files via upload

--- Data preparation.R
+++ Data preparation.R
@@ -2,13 +2,20 @@
 
 # source("Packages.R")
 
+# Set working directory to the location of the R-Script to conveniently access the folder containing the data later
+library(rstudioapi)
+setwd(dirname(getSourceEditorContext()$path))
+
 rm(list = ls())
 
 library(dplyr)
 library(data.table)
 
 # data <- read.csv("C:\\Users\\tilma\\Documents\\Uni\\Master Economics\\Machine Learning\\Statistical Machine Learning\\Data\\hour.csv")
-data <- fread(file = "C:\\Users\\tilma\\Documents\\Uni\\Master Economics\\Machine Learning\\Statistical Machine Learning\\Data\\hour.csv", sep = ",")
+# data <- fread(file = "C:\\Users\\tilma\\Documents\\Uni\\Master Economics\\Machine Learning\\Statistical Machine Learning\\Data\\hour.csv", sep = ",")
+
+# Store the Data folder in the same directory as the folder containing R scripts
+data <- fread(file = "../Data\\hour.csv", sep = ",")
 
 # Display data format
 str(data)           # all num or int except dteday which is chr
@@ -32,8 +39,9 @@ data$dteday <- NULL
 # Convert "season" to a factor with custom labels
 data$season <- factor(data$season, levels = 1:4, labels = c("Spring", "Summer", "Fall", "Winter"))
 
-# Convert "weekday" to a factor with custom labels and relevel the factor to start from Monday (1)
+# Convert "weekday" to a factor with custom labels
 data$weekday <- factor(data$weekday, levels = 0:6, labels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+# Relevel the factor to start from Monday (1)
 # data$weekday <- relevel(data$weekday, ref = "Monday")
 
 # Create dummy variables for "season" and "weekday"
@@ -48,21 +56,16 @@ colnames(weekday_dummies) <- levels(data$weekday)
 
 # Combine the dummy variables 
 data <- cbind(data, season_dummies, weekday_dummies)
+rm(list=c('season_dummies', 'weekday_dummies'))
 
 # Remove the original variables
-data <- data[, !"season", with = FALSE]
-data <- data[, !"weekday", with = FALSE]
-data <- data[, !"instant", with = FALSE]
-data <- data[, !"registered", with = FALSE] # need to exclude both as they are 
-data <- data[, !"casual", with = FALSE]     # sub-measures of the label "cnt"
-
-
-
+data <- subset(data, select = -c(season, weekday, instant, registered, casual))
 
 # Convert all variables to numeric using lapply and as.numeric
 # data[] <- lapply(data, as.numeric)
 
 # Create Test and Training dataset (80% of data) and test dataset (20% of data)
+set.seed(123456)
 training_sample <- data[,sample(.N, floor(.N*.80))]
 data_training <- data[training_sample]
 data_test <- data[-training_sample]
