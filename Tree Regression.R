638916529a299e9e4cedba0130e6deb23fb4ec86
dhongle <105487585+dhongle@users.noreply.github.com>
2023-07-31 15:36:30 +0200

    Add files via upload

--- Tree Regression.R
+++ Tree Regression.R
@@ -2,7 +2,7 @@ library(rpart)
 library(rpart.plot)
 
 # Tree regression using all dependent variable except for lagged count
-model_tree_nonlag <- rpart(data=data, formula = cnt ~ yr + mnth + hr +
+model_tree_nonlag <- rpart(data=data_training, formula = cnt ~ yr + mnth + hr +
                       holiday + workingday +
                       Spring + Summer + Fall + Winter +
                       Monday + Tuesday + Wednesday + Thursday +
@@ -13,7 +13,7 @@ prp(model_tree_nonlag)
 printcp(model_tree_nonlag)
 
 # Add lagged count
-model_tree <- rpart(formula = cnt ~ ., data=data, method="anova")
+model_tree <- rpart(formula = cnt ~ ., data=data_training, method="anova")
 prp(model_tree)
 
 # Tabular and a graphical overview of the cp parameter, which captures model complexity
@@ -28,7 +28,7 @@ prp(model_tree_prune) # new new tree is unchanged
 printcp(model_tree_prune)
 
 # Regress cnt on only hr and lag variables we will get the same tree as model_tree
-model_tree_simple <- rpart(data=data, formula = cnt ~ hr + lagged_cnt,
+model_tree_simple <- rpart(data=data_training, formula = cnt ~ hr + lagged_cnt,
                            method="anova")
 prp(model_tree_simple)
 printcp(model_tree_simple)
@@ -37,23 +37,11 @@ printcp(model_tree_simple)
 pred_tree <- predict(model_tree)
 mse_tree <- mean((data$cnt - pred_tree)^2)
 
-# Linear model
-lm_lagged <- lm(cnt ~ ., data = data)
-mse_lm_lagged <- mean(lm_lagged$residuals^2)
-
-# Create a data frame with the model names and their corresponding MSE values
-result_table <- data.frame(
-  Model = c("model_tree", "lm_lagged"),
-  MSE = c(mse_tree, mse_lm_lagged)
-)
-
-print(result_table)
-
 # Compares the true (black) and the predicted values (orange)
 
 library(ggplot2)
 
-ggplot(data=data, aes(x=lagged_cnt, y=cnt))+
+ggplot(data=data_training, aes(x=lagged_cnt, y=cnt))+
   geom_point(size=0.5) +
   geom_point(aes(x=lagged_cnt, y=pred_tree), color="#f1b147") +
   labs(
@@ -64,7 +52,7 @@ ggplot(data=data, aes(x=lagged_cnt, y=cnt))+
 
 # Plot the predictions against the true values
 ggplot()+
-  geom_point(aes(x=pred_tree, y=data$cnt)) +
+  geom_point(aes(x=pred_tree, y=data_training$cnt)) +
   geom_abline(slope = 1, intercept = 0, linetype="dashed") +
   labs(
     x = "Predicted count",
@@ -75,12 +63,44 @@ ggplot()+
 
 #-------------------------------------------------------
 
-# PCA
+# PCA with scaled dependent variable
 
-model_tree_PCA <- rpart(formula = cnt ~ ., data=data_PC18 , method="anova")
+# Get standardized data
+dt_pca <- data.table(scale(data_training))
+
+# Only features to avoid information leakage on dependent
+feature_pca <- data.table(scale(data_training[, -"cnt"]))
+model_feature_pca <- prcomp(feature_pca)
+
+# The eigenvectors
+model_feature_pca$rotation
+# The new 22 coordinates in the PC coordinate system 
+model_feature_pca$x
+
+# New training set with principal components (cnt = dt_pca$cnt)
+train_set_pca <- data.frame(cnt = dt_pca$cnt, model_feature_pca$x)
+
+# Tree model with PCA
+model_tree_PCA <- rpart(formula = cnt ~ ., data=train_set_pca, method="anova")
 prp(model_tree_PCA)
-printcp(model_tree_PCA)
 
-# Predict the number of rental bikes
+
+# Predict the number of rental bikes (mean((dt_pca$cnt...)
 pred_tree_PCA <- predict(model_tree_PCA)
 mse_tree_PCA <- mean((dt_pca$cnt - pred_tree_PCA)^2)
+
+#-----------------------------------
+
+# PCA without scaling the dependent variable
+
+# New training set with principal components (cnt = data_training$cnt)
+train_set_pca_nonscale <- data.frame(cnt = data_training$cnt, model_feature_pca$x)
+
+# Tree model with PCA
+model_tree_PCA_nonscale <- rpart(formula = cnt ~ ., data=train_set_pca_nonscale, method="anova")
+prp(model_tree_PCA_nonscale)
+
+
+# Predict the number of rental bikes (mean((data_training$cnt...)
+pred_tree_PCA_nonscale <- predict(model_tree_PCA_nonscale)
+mse_tree_PCA_nonscale <- mean((data_training$cnt - pred_tree_PCA_nonscale)^2)
